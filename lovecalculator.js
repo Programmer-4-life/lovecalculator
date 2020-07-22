9c93ae31ce7dd65af76d1e918b7e1cbb4033c075
Programmer-4-life <58579548+Programmer-4-life@users.noreply.github.com>
2020-07-22 17:04:31 +0500

    Add files via upload

---
+++ lovecalculator.js
@@ -0,0 +1,13 @@
+window.alert("Welcome to love calculator by Abdullah");
+window.prompt("What is your name?");
+window.prompt("What is your partner name?");
+var loveScore = Math.random()*100;
+loveScore = Math.floor(loveScore)+1;
+if(loveScore>70)
+{
+    alert("Your love score is " + loveScore + "%" + " You love each other like kanye loves kanye.");
+}
+else
+{
+    alert("Your love score is " + loveScore + "%" + " Tera bhi katega.");
+}
